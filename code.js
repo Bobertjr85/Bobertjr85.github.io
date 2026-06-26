927d4b95e16c6018a05309985e12048abd4df873
Bobertjr85 <64509355+Bobertjr85@users.noreply.github.com>
2026-06-26 09:47:56 -0400

    t

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ var allEvents = {};
 var userTimezone = "Etc/UTC";
 
 // only turn this on if you really like the idea of lots and lots of console spew
-const debug = true;
+const debug = false;
 // it's useful to turn this off while debugging, so you don't get spew every second
 const enableConstantUpdates = true;
 
